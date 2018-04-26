ae31b025a96c902680690ea0cbbaa3c0734b0d6a
Stefanie Habersatter <habi1986@gmx.at>
2018-04-26 23:28:38 +0200

    satisfy 11/12 tests

--- 01-fp/src/partial.js
+++ 01-fp/src/partial.js
@@ -9,4 +9,22 @@
  */
 export const _ = undefined;
 
-export function partial() {}
+export function partial(f, ...args) { 
+    const allArgs = new Array(f.length).fill(_);
+    const rec = (...args) => {
+        args.forEach((argument) => (allArgs[allArgs.indexOf(_)] = argument));
+        if (allArgs.includes(_)) return rec;
+        else return f(...allArgs);
+    }
+
+    if (allArgs.includes(_)) return rec;
+    else return f;
+}
+
+
+// https://gist.github.com/JamieMason/1228339132986291693726d11bd8dd1f
+// const pApply = (fn, ...cache) => (...args) => {
+//     const all = cache.concat(args);
+//     return all.length >= fn.length ? fn(...all) : pApply(fn, ...all);
+//   };
+  
