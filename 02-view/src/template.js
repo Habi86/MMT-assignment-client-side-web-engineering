8cffbfe57c90a59511af21fb8cc2721d334ba6d3
Stefanie Habersatter <habi1986@gmx.at>
2018-05-21 17:11:13 +0200

    satisfy first test - in ugly :D

--- 02-view/src/template.js
+++ 02-view/src/template.js
@@ -17,56 +17,58 @@
 const MATCH_ELEMENT = /<([a-z][a-z0-9]*\b[^>]*)>(.*?)<\/\1>/g;
 const MATCH_VARIABLE = /^\{\{(.+)\}\}$/;
 
+
+
+
+function createElement() {
+
+
+
+}
+
+
+
 export function build(tpl) {
+    console.log(tpl);
+
     const elem = MATCH_ELEMENT.exec(tpl);
     const txt = MATCH_VARIABLE.exec(elem[2]);
-    const nodes = new Map()
+    const nodes = new Map();
 
-    console.log("elem ########");
-    console.log(elem);
-    console.log("txt ########");
-    console.log(txt);
+    // result = document.createElement(elem[1]); // h1
+    // result.appendChild(document.createTextNode(elem[2])); // {{title}}
+
+    // let test2 = document.createElement('h1'); 
+    // test2.appendChild(document.createElement('small')).appendChild(document.createTextNode('{{title}}')); 
 
 
-    let result;
-    if (elem) {
+
+    nodes.set(txt[1], [document.createTextNode(txt[1])] )
+
+
+
+    return data => {
+        let result = {};
         result = document.createElement(elem[1]); // h1
-        result.appendChild(document.createTextNode(elem[2])); // {{title}}
-
-        nodes.set(txt[1], [document.createTextNode(txt[1])] )
-    }
-
-    // return obj => {
-    //     return {
-    //         el: result
-    //     }
-    // }
-
-
-    return function(data) {
-        console.log("data ######");
-        console.log(data);
-
-        console.log("nodes ######");
-        console.log(nodes);
 
         for(var key in data){
-            const elems = nodes.get(key);
-            console.log("elems +++");
-            console.log(elems);
+            const elems = nodes.get(key); // title
+            // console.log("##elem)");
+            // console.log(elems[0].nodeValue); // title
             for(const elem of elems){
-                console.log(elem);
-                elem.nodeValue = data[key];
+                result.appendChild(document.createTextNode(data[key]));
+
             }
           }
-
-        return {
-            el: result
-        }
+      
+        
+        return { el: result }
     }
 
+}
 
-    // console.log(tpl);
+
+// console.log(tpl);
     // var match = MATCH_ELEMENT.exec(tpl);
     // if (!match) console.log('no match');
     // for (var c = 1; c < match.length; c++) {
@@ -83,6 +85,4 @@ export function build(tpl) {
     //     console.log('matching group is: ' + (c - 1));
     // }
         
-}
-
 
