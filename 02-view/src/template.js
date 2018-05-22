6060fda50e4eb5d0848975d67010cb8f7daa2565
Stefanie Habersatter <habi1986@gmx.at>
2018-05-22 21:42:42 +0200

    satisfy all tests except performance

--- 02-view/src/template.js
+++ 02-view/src/template.js
@@ -13,27 +13,131 @@
  * update({title: 'Hallo Welt!'});
  * el.outerHTML // <h1>Hallo, Welt!</h1>
  */
-
-const MATCH_ELEMENT = /<([a-z][a-z0-9]*\b[^>]*)>(.*?)<\/\1>/g;
+// const MATCH_ELEMENT = /<([a-z][a-z0-9]*\b[^>]*)>(.*?)<\/\1>/g;
+const MATCH_ELEMENT = /<([a-z][a-z0-9]*\b[^>]*)>(.*?)<\/\1>/;
 const MATCH_VARIABLE = /^\{\{(.+)\}\}$/;
 
+function setVariable(data, nodes, currentNode) {
+    for (var key in data) {
+        const elems = nodes.get(key); 
 
+        while (currentNode.firstChild) currentNode.removeChild(currentNode.firstChild);
 
+        for (const elem of elems) {
+            currentNode.appendChild(document.createTextNode(data[key]));
+        }
+    }
+}
 
-function createElement() {
+export function build(tpl) {
+    let result = null;
+    let currentNode = null;
+    let input = tpl;
+    let element = null;
+    let deeper = true;
 
+    while (deeper) {
+        const newElement = MATCH_ELEMENT.exec(input);
 
+        if (newElement) {
+            element = newElement;
 
+            if (!result) {
+                result = document.createElement(element[1]);
+                currentNode = result;
+
+            } else {
+                const newNode = document.createElement(element[1]);
+                currentNode.appendChild(newNode);
+                currentNode = newNode;
+
+            }
+        } else {
+            deeper = false;
+
+        }
+        input = element[2];
+
+    }
+    let txt = MATCH_VARIABLE.exec(element[2]);
+    const nodes = new Map();
+    nodes.set(txt[1], [document.createTextNode(txt[1])]);
+
+    return data => {
+        setVariable(data, nodes, currentNode);
+
+        return {
+            el: result,
+            update: (data) => {setVariable(data, nodes, currentNode);}
+        }
+    };
 }
 
 
 
-export function build(tpl) {
-    console.log(tpl);
+/*
+function createElement(tpl, nodes) {
+    const MATCH_ELEMENT = /<([a-z][a-z0-9]*\b[^>]*)>(.*?)<\/\1>/g;
+    const MATCH_VARIABLE = /^\{\{(.+)\}\}$/;
+    
+    let [ string, tag, input ] = MATCH_ELEMENT.exec(tpl); // <h1>{{title}}</h1>   h1  {{title}}
+    
+    // console.log("#######");
+    // console.log(string);    
+    // console.log(tag);
+    // console.log(input);    
+    // console.log("####### end");
 
-    const elem = MATCH_ELEMENT.exec(tpl);
-    const txt = MATCH_VARIABLE.exec(elem[2]);
-    const nodes = new Map();
+    // nodes = document.createElement(tag);
+    // nodes.appendChild(document.createTextNode(input));
+
+    console.log("nodes###2);");
+    console.log(nodes);
+
+    if (MATCH_VARIABLE.exec(input) == null) { // if tags are still found
+        console.log("hier dürfte i ned rein beim 1. test");
+        nodes = document.createElement(tag);
+        return createElement(input, nodes);
+
+    } else {
+        let [ variable, txt ] = MATCH_VARIABLE.exec(input); // {{title}}    title
+        // console.log(variable);
+        // console.log(txt);    
+		// let node = document.createElement(tag);
+		// let textNode = document.createTextNode(nodes.get(txt));
+        // node.appendChild(textNode);    
+
+        let node = document.createElement(tag); 
+        node.appendChild(document.createTextNode(variable)); 
+    
+
+
+
+        if (nodes) {
+           nodes.appendChild(node);
+           console.log("if nodes is true da bin i ");
+        } else {
+            nodes = node;
+        }
+
+        //nodes.appendChild(document.createTextNode(input));
+        console.log("###");
+        console.log(nodes);
+    
+        return { el: nodes };
+    
+    }
+
+}
+
+export function build(tpl) {
+    let nodes = new Map();
+    let result = createElement(tpl, nodes); //new result here
+    // console.log(nodes);
+
+    // const elem = MATCH_ELEMENT.exec(tpl);
+    // const txt = MATCH_VARIABLE.exec(elem[2]);
+    // const nodes1 = new Map();
 
     // result = document.createElement(elem[1]); // h1
     // result.appendChild(document.createTextNode(elem[2])); // {{title}}
@@ -43,20 +147,21 @@ export function build(tpl) {
 
 
 
-    nodes.set(txt[1], [document.createTextNode(txt[1])] )
+    // nodes1.set(txt[1], [document.createTextNode(txt[1])] )
 
 
 
     return data => {
-        let result = {};
-        result = document.createElement(elem[1]); // h1
+        // let result = {};
+        // result = document.createElement(elem[1]); // h1
 
         for(var key in data){
-            const elems = nodes.get(key); // title
+            const elems = nodes1.get(key); // title
             // console.log("##elem)");
             // console.log(elems[0].nodeValue); // title
             for(const elem of elems){
-                result.appendChild(document.createTextNode(data[key]));
+                // result.appendChild(document.createTextNode(data[key]));
+                elem.nodeValue = data[key];
 
             }
           }
@@ -67,7 +172,7 @@ export function build(tpl) {
 
 }
 
-
+*/
 // console.log(tpl);
     // var match = MATCH_ELEMENT.exec(tpl);
     // if (!match) console.log('no match');
@@ -84,5 +189,5 @@ export function build(tpl) {
     //     if (!match2[c]) continue;
     //     console.log('matching group is: ' + (c - 1));
     // }
-        
+
 
